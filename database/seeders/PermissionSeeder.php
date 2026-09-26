3f253ac0be347b65f73fd43543ca66b5cf84d182
afgan galih <rhopaloceus@gmail.com>
2026-09-26 11:12:47 +0700

    fix(tenant/outbound): align route permission middleware with RBAC naming scheme

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -107,4 +107,3 @@ class PermissionSeeder extends Seeder
         }
     }
 }
-
--- routes/web/tenant/outbound.php
+++ routes/web/tenant/outbound.php
@@ -5,26 +5,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('{tenant_slug}')->middleware(['identify_tenant', 'auth', 'verified'])->group(function () {
     Route::get('/transactions/outbound', [OutboundController::class, 'index'])
-        ->middleware('permission:outbound.view')
+        ->middleware('permission:tenant.outbound.view')
         ->name('tenant.transactions.outbound.index');
 
     Route::get('/transactions/outbound/{manifest_code}', [OutboundController::class, 'show'])
-        ->middleware('permission:outbound.view')
+        ->middleware('permission:tenant.outbound.show')
         ->name('tenant.transactions.outbound.show');
 
     Route::post('/transactions/outbound/verify-barcode', [OutboundController::class, 'verifyBarcode'])
-        ->middleware('permission:outbound.edit')
+        ->middleware('permission:tenant.outbound.edit')
         ->name('tenant.transactions.outbound.verify-barcode');
 
     Route::post('/transactions/outbound/packing', [OutboundController::class, 'updatePacking'])
-        ->middleware('permission:outbound.edit')
+        ->middleware('permission:tenant.outbound.edit')
         ->name('tenant.transactions.outbound.update-packing');
 
     Route::post('/transactions/outbound/commit', [OutboundController::class, 'commit'])
-        ->middleware('permission:outbound.commit')
+        ->middleware('permission:tenant.outbound.commit')
         ->name('tenant.transactions.outbound.commit');
 
     Route::post('/transactions/outbound/hold', [OutboundController::class, 'hold'])
-        ->middleware('permission:outbound.edit')
+        ->middleware('permission:tenant.outbound.hold')
         ->name('tenant.transactions.outbound.hold');
 });
